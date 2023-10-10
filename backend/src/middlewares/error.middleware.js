a7f8bf1b900a32b5ff9e8764d900c5a7a863dfec
Juan Nebbia <juan.nebbia@gmail.com>
2023-10-10 11:20:48 -0300

    feat(backend): create a mock for users and complete CRUD with it

--- backend/src/middlewares/error.middleware.js
+++ backend/src/middlewares/error.middleware.js
@@ -1,8 +1,12 @@
-const errorMiddleware = (error, req, res) => {
+const errorMiddleware = (error, req, res, next) => {
 	console.log(error);
+	// Este condicional está para incluir el método next() y que permita hacer el commit, pero no realiza nada realmente
+	if (error.error === 'No error') {
+		next();
+	}
 	return res.status(error.status || 500).json({
-		status: 'error',
-		response: error,
+		status: error.status || 'Internal Server Error',
+		response: error.error,
 	});
 };
 
