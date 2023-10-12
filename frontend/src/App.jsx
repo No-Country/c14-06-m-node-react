a98b2244c14857ddb95146ea24279096aeca53d8
ruben <r.alejandro.gn@gmail.com>
2023-10-11 21:42:48 -0300

    style(web): :sparkles: added home, signup and register pages. plus navBar and footer
    Creadas paginas home, registro y login. navbar+ footer. Instale dependencia de FontAwesome

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,30 @@
 import { Routes, Route } from 'react-router-dom';
 import Categories from './components/Categories';
 import ProfesionalsList from './components/ProfesionalsList';
+import ComoFunciona from './pages/ComoFunciona';
+import Home from './pages/Home';
+import CreateAccount from './pages/CreateAccount';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import SignIn from './pages/SignIn';
 
 const App = () => {
 	return (
-		<Routes>
-			<Route path="/" element={<Categories />} />
-			<Route
-				path="/profesionalsList/:categoryTitle"
-				element={<ProfesionalsList />}
-			/>
-		</Routes>
+		<>
+			<Navbar />
+			<Routes>
+				<Route path="/categorias" element={<Categories />} />
+				<Route
+					path="/profesionalsList/:categoryTitle"
+					element={<ProfesionalsList />}
+				/>
+				<Route path="/" element={<Home />} />
+				<Route path="/como-funciona" element={<ComoFunciona />} />
+				<Route path="/crear-cuenta" element={<CreateAccount />} />
+				<Route path="/iniciar-sesion" element={<SignIn />} />
+			</Routes>
+			<Footer />
+		</>
 	);
 };
 
